67b6a79c15fd0f60cc713e29beb9fcdf5108f0e4
prosper oforah <111814126+pio001@users.noreply.github.com>
2025-10-21 20:01:18 +0100

    Delete server directory because its has a new repo and hosted in render

--- server/api/[...path].js
+++
@@ -1,6 +0,0 @@
-const serverless = require('serverless-http');
-const app = require('../server.js');
-
-const handler = serverless(app, { basePath: '/api' });
-
-module.exports = (req, res) => handler(req, res);
